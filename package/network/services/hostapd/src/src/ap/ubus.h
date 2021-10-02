690ff1962f92f20727a9462d5a2a5afe8f8b5f1b
lunaticurey <125438787@qq.com>
2021-10-03 01:05:30 +0800

    hostapd: expose beacon reports through ubus
    Subscribe to beacon reports through ubus.
    Can be used for hearing map and client steering purposes.
    
    First enable rrm:
        ubus call hostapd.wlan0 bss_mgmt_enable '{"beacon_report":True}'
    
    Subscribe to the hostapd notifications via ubus.
    
    Request beacon report:
        ubus call hostapd.wlan0 rrm_beacon_req
    	'{"addr":"00:xx:xx:xx:xx:xx", "op_class":0, "channel":1,
    	"duration":1,"mode":2,"bssid":"ff:ff:ff:ff:ff:ff", "ssid":""}'
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    [rework identation]
    Signed-off-by: David Bauer <mail@david-bauer.net>
    
    Signed-off-by: lunaticurey <125438787@qq.com>

--- package/network/services/hostapd/src/src/ap/ubus.h
+++ package/network/services/hostapd/src/src/ap/ubus.h
@@ -25,6 +25,7 @@ struct hostapd_ubus_request {
 
 struct hostapd_iface;
 struct hostapd_data;
+struct rrm_measurement_beacon_report;
 
 #ifdef UBUS_SUPPORT
 
@@ -44,7 +45,10 @@ void hostapd_ubus_free_bss(struct hostapd_data *hapd);
 
 int hostapd_ubus_handle_event(struct hostapd_data *hapd, struct hostapd_ubus_request *req);
 void hostapd_ubus_notify(struct hostapd_data *hapd, const char *type, const u8 *mac);
-
+void hostapd_ubus_notify_beacon_report(struct hostapd_data *hapd,
+				       const u8 *addr, u8 token, u8 rep_mode,
+				       struct rrm_measurement_beacon_report *rep,
+				       size_t len);
 #else
 
 struct hostapd_ubus_bss {};
@@ -71,6 +75,14 @@ static inline int hostapd_ubus_handle_event(struct hostapd_data *hapd, struct ho
 }
 
 static inline void hostapd_ubus_notify(struct hostapd_data *hapd, const char *type, const u8 *mac)
+{
+}
+
+static inline void hostapd_ubus_notify_beacon_report(struct hostapd_data *hapd,
+						     const u8 *addr, u8 token,
+						     u8 rep_mode,
+						     struct rrm_measurement_beacon_report *rep,
+						     size_t len)
 {
 }
 #endif
