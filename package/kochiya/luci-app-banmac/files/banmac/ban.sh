8f3f9156fe75486bd3020468b84ded554c81db6c
lunaticurey <125438787@qq.com>
2021-11-18 15:13:52 +0800

    luci-app-banmac:add log lock

--- package/kochiya/luci-app-banmac/files/banmac/ban.sh
+++ package/kochiya/luci-app-banmac/files/banmac/ban.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-
+LOCK_FILE="/var/lock/procd_act_banmac.lock"
 sectionname=$(echo $0 | cut -d '_' -f 2 | sed 's/^OO!%!OO//')
 getcmac=$(uci get banmac.$sectionname.banlist_mac | tr 'A-Z' 'a-z')
+hostname=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 4)
+hostip=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 3)
+
+function add_lock(){
+	exec 1000>"$LOCK_FILE"
+	flock -xn 1000
+}
+
+function del_lock() {
+	flock -u 1000
+	rm -rf "$LOCK_FILE"
+}
+
+function act_banmac(){
+
 iptables -I FORWARD -m mac --mac-source $getcmac -j DROP
 
 for i in $(seq 0 1)
@@ -13,3 +28,13 @@ do
 		fi
 	done
 done
+}
+
+function ban_log(){
+echo "★禁网设备：$hostname($hostip) MAC地址：$getcmac 操作日期：$(date +%Y年%m月%d日\ %H点%M分%S秒)" >> /etc/banmacdetails
+}
+
+add_lock
+act_banmac
+ban_log
+del_lock
--- package/kochiya/luci-app-banmac/files/banmac/unban.sh
+++ package/kochiya/luci-app-banmac/files/banmac/unban.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-
+LOCK_FILE="/var/lock/procd_act_unbanmac.lock"
 sectionname=$(echo $0 | cut -d '_' -f 2 | sed 's/^OO!%!OO//')
 getcmac=$(uci get banmac.$sectionname.banlist_mac | tr 'A-Z' 'a-z')
-iptables -D FORWARD -m mac --mac-source $getcmac -j DROP
+
+function add_lock(){
+	exec 1000>"$LOCK_FILE"
+	flock -xn 1000
+}
+
+function del_lock() {
+	flock -u 1000
+	rm -rf "$LOCK_FILE"
+}
+
+function act_unbanmac(){
+	iptables -D FORWARD -m mac --mac-source $getcmac -j DROP
+}
+
+function del_banlog(){
+	sed -i "s/$(cat /etc/banmacdetails | grep $getcmac | head -n 1)//;/^\s*$/d" /etc/banmacdetails
+}
+
+add_lock
+act_unbanmac
+del_banlog
+del_lock
