75c6d1a5e1be252349b9b205eb1f94cc38e4d264
lunatickochiya <125438787@qq.com>
2025-03-30 08:27:09 +0800

    ar71xx:dw33d-wifi fix

--- target/linux/ar71xx/files/arch/mips/ath79/mach-dw33d-wifi.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-dw33d-wifi.c
@@ -51,6 +51,7 @@
 #define DW33D_WIFI_MAC1_OFFSET		6
 #define DW33D_WIFI_WMAC_OFFSET		12
 #define DW33D_WIFI_WMAC_CALDATA_OFFSET	0x1000
+#define DW33D_WIFI_PCIE_CALDATA_OFFSET	0x5000
 
 static struct gpio_led dw33d_wifi_leds_gpio[] __initdata = {
 	{
@@ -91,14 +92,14 @@ static struct gpio_keys_button dw33d_wifi_gpio_keys[] __initdata = {
 	}
 };
 
-/* GMAC0 of the QCA8337 switch is connected to the QCA9558 SoC via SGMII */
-static struct ar8327_pad_cfg dw33d_wifi_qca8337_pad0_cfg = {
+/* GMAC6 of the QCA8337 switch is connected to the QCA9558 SoC via SGMII */
+static struct ar8327_pad_cfg dw33d_wifi_qca8337_pad6_cfg = {
 	.mode = AR8327_PAD_MAC_SGMII,
 	.sgmii_delay_en = true,
 };
 
-/* GMAC6 of the QCA8337 switch is connected to the QCA9558 SoC via RGMII */
-static struct ar8327_pad_cfg dw33d_wifi_qca8337_pad6_cfg = {
+/* GMAC0 of the QCA8337 switch is connected to the QCA9558 SoC via RGMII */
+static struct ar8327_pad_cfg dw33d_wifi_qca8337_pad0_cfg = {
 	.mode = AR8327_PAD_MAC_RGMII,
 	.txclk_delay_en = true,
 	.rxclk_delay_en = true,
