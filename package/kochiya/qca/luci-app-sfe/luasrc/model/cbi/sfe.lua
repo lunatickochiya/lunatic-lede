15da356d675ad979607c4040a8917e563038ee35
lunatickochiya <125438787@qq.com>
2025-03-31 19:42:43 +0800

    qca:use higarfield ver

--- package/kochiya/qca/luci-app-sfe/luasrc/model/cbi/sfe.lua
+++ package/kochiya/qca/luci-app-sfe/luasrc/model/cbi/sfe.lua
@@ -1,23 +1,36 @@
+local m, s, enabled, bridge, offload_at_pkts
+local SYS = require "luci.sys"
+
+if SYS.call("lsmod | grep -qw \'^fast_classifier\'") == 0 then
+    Status = translate("<strong><font color=\"green\">Shortcut forwarding engine is running</font></strong>")
+else
+    Status = translate("<strong><font color=\"red\">Shortcut forwarding engine is not running</font></strong>")
+end
 
 m = Map("sfe")
-m.title	= translate("Qualcomm Shortcut FE")
+m.title = translate("Shortcut Forwarding Engine Acceleration Settings")
 m.description = translate("Opensource Qualcomm Shortcut FE driver (Fast Path)")
 
-m:append(Template("lunatic-sfe/status"))
-
-s = m:section(TypedSection, "sfe", "")
+s = m:section(NamedSection, "config", "sfe", "")
 s.addremove = false
 s.anonymous = true
+s.description = string.format("%s<br /><br />", Status)
 
-enable = s:option(Flag, "enabled", translate("Enable"))
-enable.default = 0
-enable.rmempty = false
-enable.description = translate("Enable Fast Path offloading for connections. (decrease cpu load / increase routing throughput)")
+enabled = s:option(Flag, "enabled", translate("Enable"))
+enabled.rmempty = false
 
-bridge = s:option(Flag, "bridge", translate("Bridge Acceleration"))
-bridge.default = 0
-bridge.rmempty = false
-bridge.description = translate("Enable Bridge Acceleration (may be functional conflict with bridge-mode VPN Server)")
-bridge:depends("enabled", 1)
+bridge = s:option(Flag, "bridge", translate("Bridge acceleration"))
+bridge.description = translate("Enable bridge acceleration")
+bridge:depends("enabled", "1")
+
+offload_at_pkts = s:option(Value, "offload_at_pkts", translate("Accelerate at packets"))
+offload_at_pkts.description = translate("Start acceleration after how many packets, defaultly 128, at least 4")
+offload_at_pkts.datatype = "and(uinteger,min(4))"
+offload_at_pkts.placeholder = 128
+offload_at_pkts:depends("enabled", "1")
+
+function m.on_after_commit(self)
+    os.execute("/etc/init.d/sfe restart >/dev/null 2>&1 &")
+end
 
 return m
